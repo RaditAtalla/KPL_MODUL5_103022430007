edbef282663f913f0b01ae70ecf7fe775ed8b42e
RaditAtalla <radityaderachmadie@gmail.com>
2026-03-31 07:33:12 +0700

    implementasi generic class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿SimpleDatabase<int> sd = new SimpleDatabase<int>();
+sd.AddNewData(10);
+sd.AddNewData(30);
+sd.AddNewData(22);
+sd.PrintAllData();
+
+public class SimpleDatabase<T>
+{
+    List<T> storedData;
+    List<DateTime> inputDates;
+
+    public SimpleDatabase()
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i+1) + " berisi: " + storedData[i] +
+                            ", yang disimpan pada waktu UTC: " + inputDates[i]);
+        }
+    }
+}
